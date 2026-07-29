596736a05139580422554cd481c8721c3d0b6605
farhanKhan011 <farhank023102@gmail.com>
2026-07-30 02:42:59 +0500

    I learned , Array Iterating ,also enumuration

---
+++ arrayIterating.py
@@ -0,0 +1,123 @@
+# ---------------Iterating Arrays---------------------
+'''
+Iterating Arrays
+Iterating means going through elements one by one.
+As we deal with multi-dimensional arrays in numpy, we can do this using basic for loop of python.
+If we iterate on a 1-D array it will go through each element one by one.
+'''
+# Iterate on the elements of the following 1-D array:
+# import numpy as np 
+# arr = np.array([1,2,3,4,5,6])
+# for i in arr:
+#     print(i)
+'''
+Iterating 2-D Arrays
+In a 2-D array it will go through all the rows.
+Example:
+Iterate on the elements of the following 2-D array:
+'''
+# import numpy as np
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# for x in arr:
+#     print(x)
+'''If we iterate on a n-D array it will go through (n-1th) dimension one by one.
+ To return the actual values, the scalars, we have to iterate the arrays in each dimension.
+ Iterate on each scalar element of the 2-D array:'''
+# import numpy as np
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# for i in arr:
+#     for j in i:
+#         print(j)
+
+'''
+Iterating 3-D Arrays
+In a 3-D array it will go through all the 2-D arrays.
+Example:
+Iterate on the elements of the following 3-D array:
+'''
+# import numpy as np
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# for x in arr:
+    # print(x)
+# To return the actual values, the scalars, we have to iterate the arrays in each dimension.
+# for i in arr:
+#     for j in i:
+#         for k in j:
+#             print(k)
+
+# ________________________________________________
+'''
+Iterating Arrays Using nditer()
+The function nditer() is a helping function that can be used from very basic to very advanced iterations. It solves some basic issues which we face in iteration, lets go through it with examples.
+
+Iterating on Each Scalar Element
+In basic for loops, iterating through each scalar of an array we need to use n for loops which can be difficult to write for arrays with very high dimensionality.
+'''
+# Iterate through the following 3-D array:
+# import numpy as np
+# arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# for x in np.nditer(arr):
+#     print(x)
+# _________________________________________________________
+'''
+Iterating Array With Different Data Types
+We can use op_dtypes argument and pass it the expected datatype to change the datatype of elements while iterating.
+NumPy does not change the data type of the element in-place (where the element is in array) so it needs some other space to perform this action, that extra space is called buffer, and in order to enable it in nditer() we pass flags=['buffered'].
+Example
+Iterate through the array as a string:
+'''
+# import numpy as np
+# arr = np.array([1, 2, 3])
+# for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+#     print(x)
+
+'''
+Iterating With Different Step Size
+We can use filtering and followed by iteration.
+Example
+Iterate through every scalar element of the 2D array skipping 1 element:
+'''
+# import numpy as np
+# arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# for x in np.nditer(arr[:,::2]):
+#     print(x)
+
+# ____________________
+'''
+Enumerated Iteration Using ndenumerate()
+Enumeration means mentioning sequence number of somethings one by one.
+
+Sometimes we require corresponding index of the element while iterating, the ndenumerate() method can be used for those usecases.
+
+Example
+Enumerate on following 1D arrays elements:
+'''
+# import numpy as np
+# arr = np.array([1, 2, 3])
+# for idx, x in np.ndenumerate(arr):
+#   print(idx, x)
+# Enumerate on following 2D array's elements:
+# import numpy as np
+# arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# for idx, x in np.ndenumerate(arr):
+#     print(idx,x)
+
+
+
+# Array Iterating Completed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
