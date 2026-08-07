61875737e6637f9efbef985461dc2cf61af101c4
farhankhan011 <farhank023102@gmail.com>
2026-08-07 12:02:42 -0700

    Array Search completed

--- arraySearch.py
+++ arraySearch.py
@@ -3,13 +3,104 @@
 
 # To search an array, use the where() method.
 
-# ExampleGet your own Python Server
+# Example:
 # Find the indexes where the value is 4:
+
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5, 4, 4])
+# x = np.where(arr == 4 )
+# print(x)
+
+# The example above will return a tuple: (array([3, 5, 6],)
+# Which means that the value 4 is present at index 3, 5, and 6.
+
+# Example
+# Find the indexes where the values are odd:
+'''import numpy as np
+arr = np.array([10, 14, 93, 41, 8, 7]) 
+x = np.where(arr % 2 != 0 )
+print(x)
+'''
+
+# Find the indexes where the values are even:   
+'''
 
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 4, 4])
+arr = np.array([10, 14, 93, 41, 8, 7])
 
-x = np.where(arr == 4)
+x = np.where(arr%2 == 0)
 
 print(x)
+'''
+'''
+There is a method called searchsorted() which performs a binary search in the array, and returns the index where the specified value would be inserted to maintain the search order.
+The searchsorted() method is assumed to be used on sorted arrays.
+Example:
+Find the indexes where the value 7 should be inserted:
+# '''
+# import numpy as np
+# arr = np.array([6, 7, 8, 9])
+# sortedArr = np.searchsorted(arr, 7 )
+# print(sortedArr)  
+'''
+# the output is 7 , here by the binary search mean unlike the linear search from left to right searching, the binary search find the desire data like 7 in here by splitting the data in the middle and match if not equal to the side , so the side is truncated and search in the remaining side and apply the same method until the match result like 7 is finded 
+
+# Example explained: The number 7 should be inserted on index 1 to remain the sort order.
+
+# The method starts the search from the left and returns the first index where the number 7 is no longer larger than the next value.
+'''
+'''
+Search From the Right Side
+By default the left most index is returned, but we can give side='right' to return the right most index instead.
+
+Example
+Find the indexes where the value 7 should be inserted, starting from the right:
+'''
+# import numpy as np
+# arr = np.array([6, 7, 8, 9])
+# x = np.searchsorted(arr, 7, side='right')
+# print(x)
+
+# Example explained: The number 7 should be inserted on index 2 to remain the sort order.
+
+# The method starts the search from the right and returns the first index where the number 7 is no longer less than the next value.
+'''
+Multiple Values
+To search for more than one value, use an array with the specified values.
+
+Example
+Find the indexes where the values 2, 4, and 6 should be inserted:
+'''
+import numpy as np
+arr = np.array([1, 3, 5, 7])
+x = np.searchsorted(arr, [2, 4, 6])
+print(x)
+# The return value is an array: [1 2 3] containing the three indexes where 2, 4, 6 would be inserted in the original array to maintain the order.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
