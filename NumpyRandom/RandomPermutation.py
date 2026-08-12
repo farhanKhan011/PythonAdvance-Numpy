4774c6abc34e5d3f58e5cf7854b3a4f0a02e2eb4
farhankhan011 <farhank023102@gmail.com>
2026-08-12 17:22:48 +0500

    Random Permutation Completed in Numpy

---
+++ NumpyRandom/RandomPermutation.py
@@ -0,0 +1,30 @@
+# -------------Numpy Random Permutation----------------
+
+'''
+Random Permutations of Elements
+A permutation refers to an arrangement of elements. e.g. [3, 2, 1] is a permutation of [1, 2, 3] and vice-versa.
+The NumPy Random module provides two methods for this: shuffle() and permutation().
+Shuffling Arrays
+Shuffle means changing arrangement of elements in-place. i.e. in the array itself.
+'''
+# Randomly shuffle elements of following array:
+# import numpy as np 
+# from numpy import random 
+# arr = np.array([1,2,3,4,5])
+# random.shuffle(arr)
+# print(arr)
+# The shuffle() method makes changes to the original array.
+
+'''
+Generating Permutation of Arrays
+'''
+# Generate a random permutation of elements of following array:
+# import numpy as np 
+# from numpy import random
+# arr = np.array([1,2,3,4,5])
+# permutedArr = random.permutation(arr)
+# print(arr)
+# print(permutedArr)
+# The permutation() method returns a re-arranged array (and leaves the original array un-changed).
+
+# -------------------Random Permutation Completed-------------------
