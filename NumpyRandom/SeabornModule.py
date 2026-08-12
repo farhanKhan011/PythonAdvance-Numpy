e1f7f62dba05e0556971128cca4f7dee0a1ee07b
farhankhan011 <farhank023102@gmail.com>
2026-08-12 18:12:23 +0500

    Numpy Seaborn Module learned , Completed

---
+++ NumpyRandom/SeabornModule.py
@@ -0,0 +1,45 @@
+# ---------------Numpy Seaborn Module/ Seaborn Library ------------------
+
+'''
+Visualize Distributions With Seaborn
+Seaborn is a library that uses Matplotlib underneath to plot graphs. It will be used to visualize random distributions.
+'''
+# am using vs code, the command to install seaborn -> pip install seaborn
+
+'''
+Displots
+Displot stands for distribution plot, it takes as input an array and plots a curve corresponding to the distribution of points in the array.
+Import Matplotlib
+Import the pyplot object of the Matplotlib module in your code using the following statement:
+import matplotlib.pyplot as plt
+
+Import Seaborn
+Import the Seaborn module in your code using the following statement:
+import seaborn as sns
+'''
+# Plotting a Displot
+# import matplotlib.pyplot as plt 
+# import seaborn as sns
+# sns.displot([1,2,3,4,5])
+# plt.show()
+
+'''
+Plotting a Displot Without the Histogram
+Example
+'''
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.displot([0, 1, 2, 3, 4, 5], kind="kde")
+# plt.show()
+
+# Note: We will be using: sns.displot(arr, kind="kde") to visualize random distributions in this tutorial.
+
+# ----------Seaborn Module completed----------------
+
+
+
+
+
+
+
+
