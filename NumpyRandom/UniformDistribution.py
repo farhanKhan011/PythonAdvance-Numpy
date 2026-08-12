b7a2ec21f6b8e8a5216cf0f5155d1f0797aa3d93
farhankhan011 <farhank023102@gmail.com>
2026-08-13 02:00:00 +0500

    Learning Uniform Distribution Completed in Numpy

---
+++ NumpyRandom/UniformDistribution.py
@@ -0,0 +1,24 @@
+# ------------------Uniform Distribution-------------
+
+'''
+Uniform Distribution
+Used to describe probability where every event has equal chances of occuring.
+E.g. Generation of random numbers.
+It has three parameters:
+low - lower bound - default 0.0
+high - upper bound - default 1.0
+size - The shape of the returned array
+'''
+# Create a 2x3 uniform distribution sample:
+# from numpy import random
+# x = random.uniform( size=(2,3))
+# print(x)
+
+# Visualization of Uniform Distribution
+# from numpy import random 
+# import matplotlib.pyplot as plt 
+# import seaborn as sns
+# sns.displot(random.uniform(size=1000),kind="kde")
+# plt.show()
+
+# -----------Uniform Distribution Completed---------------
