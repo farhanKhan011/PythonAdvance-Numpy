87460f77d070060117029f2c65af3621488fa311
farhankhan011 <farhank023102@gmail.com>
2026-08-13 13:18:32 +0500

    Rayleigh Distribution Completed in Numpy

---
+++ NumpyRandom/RayleighDistribution.py
@@ -0,0 +1,20 @@
+# -----------------Rayleigh Distribution---------------
+
+'''
+Rayleigh distribution is used in signal processing.
+It has two parameters:
+scale - (standard deviation) decides how flat the distribution will be default 1.0).
+size - The shape of the returned array.
+'''
+# from numpy import random
+# x = random.rayleigh(scale = 2 , size = (2,3))
+# print(x)
+
+# Visual Distribution of Rayliegh Distribution
+# from numpy import random
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.displot(random.rayleigh(size=1000),kind='kde')
+# plt.show()
+
+# -----------Rayleigh Distribution Completed---------------
